07987978cafda803025b27a2c6a5caa55bc772dc
bizexo <41581712+bizexo@users.noreply.github.com>
2020-03-23 14:03:05 +0530

    arkedge-23-03-2020

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.scss']
+  styleUrls: ['./client.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class ClientComponent implements OnInit {
 
@@ -13,13 +15,18 @@ export class ClientComponent implements OnInit {
     {clientimg: 'client'},
     {clientimg: 'client'},
     {clientimg: 'client'},
-    {clientimg: 'client'},
-    {clientimg: 'client'},
-    {clientimg: 'client'},
     {clientimg: 'client'}
   ];
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  open(content) {
+    this.modalService.open(content);
+  }
 
   ngOnInit() {
   }
