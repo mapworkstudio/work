6630d46fd413577f6516914addc5882bb10d3611
bizexo <41581712+bizexo@users.noreply.github.com>
2020-04-02 12:38:05 +0530

    arkedge-14-3-2020

--- src/app/advancesolution/advancesolution.component.ts
+++ src/app/advancesolution/advancesolution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-advancesolution',
@@ -6,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advancesolution.component.scss']
 })
 export class AdvancesolutionComponent implements OnInit {
-
-  constructor() { }
+  constructor(private titleService: Title) { }
 
   ngOnInit() {
   }
